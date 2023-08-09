27541041f6fbab9b5459d8dae8a33acdd750678c
Dennis Silva <dev.dennissilva@gmail.com>
2023-08-09 13:05:27 +0100

    Authentication and Error 404 components added

---
+++ src/components/ForgotPassword.svelte
@@ -0,0 +1,8 @@
+
+<div class="card p-6 space-y-6 text-left shadow-xl">
+	<label class="label">
+		<span>Email</span>
+		<input type="email" placeholder="your-email@example.com" class="input" />
+	</label>
+	<button class="btn variant-filled-primary w-full">Reset Password</button>
+</div>
